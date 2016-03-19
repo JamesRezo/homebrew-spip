1feef5109772e26d5728291f25d600af6d3b1d6c
JamesRezo <james@rezo.net>
2016-03-19 22:58:57 +0100

    upgrade 1.0.0-beta3

--- Formula/spip_svn_loader.rb
+++ Formula/spip_svn_loader.rb
@@ -1,9 +1,9 @@
 class SpipSvnLoader < Formula
   desc "Installer et mettre à jour SPIP avec SVN"
   homepage "https://github.com/JamesRezo/spip_svn_loader"
-  url "https://github.com/JamesRezo/spip_svn_loader/archive/1.0.0-beta2.tar.gz"
-  version "1.0.0-beta2"
-  sha256 "7181acace0b395e706bd36a6d5a1ae5c77b8b54349aaa4e2cf7ae176bdc33caf"
+  url "https://github.com/JamesRezo/spip_svn_loader/archive/1.0.0-beta3.tar.gz"
+  version "1.0.0-beta3"
+  sha256 "061ff3d163b0444e4c651a7cefed7422722f6bf5730377b14263d37727f571fc"
   head "https://github.com/JamesRezo/spip_svn_loader.git"
 
   depends_on "subversion"
